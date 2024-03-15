6bec5b124424bf73d8f136215404a7062f322693
Steven Filbert <117874899+filsteven11@users.noreply.github.com>
2024-03-15 08:50:49 +0700

    Add files via upload

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println("siapa aku ya ?")
-	a=b-c
+	router := mux.NewRouter()
+
+	// Endpoint untuk pengguna
+	router.HandleFunc("/users", controllers.GetAllUsers).Methods("GET")
+
+	// Endpoint untuk produk
+	router.HandleFunc("/products", controllers.GetAllProducts).Methods("GET")
+
+	// Endpoint untuk transaksi
+	router.HandleFunc("/transactions", controllers.GetAllTransactions).Methods("GET")
+
+	http.Handle("/", router)
+	fmt.Println("Connected to port 8888")
+	log.Println("Connected to port 8888")
+	log.Fatal(http.ListenAndServe(":8888", router))
 }
